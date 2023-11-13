2d84055ba3041891bc1b1ae61470a278029e9791
Falon <47285315+falOn-Dev@users.noreply.github.com>
2023-11-12 23:06:16 -0500

    Initial commit

---
+++ src/main/kotlin/Lesson1_1/Lesson1-1.kt
@@ -0,0 +1,12 @@
+package Lesson1_1
+
+fun main() {
+    /*
+    Your instructions are to:
+    - Create a non-constant variable
+    - Create a constant variable
+    - Create a variable of type String
+    - Create a NULLABLE variable of type boolean, that is equal to null
+    - Print the value of each variable
+    */
+}
---
+++ src/main/kotlin/Lesson1_X/Lesson1-2.kt
@@ -0,0 +1,6 @@
+package Lesson1_X
+
+/*
+Create a function that takes two integers as arguements and returns the sum of those two integers.
+Then create a main function that asks the user for two integers and prints the sum of those two integers.
+ */
