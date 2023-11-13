72243fd850aa7bb773dc71df876f5414dcac3364
Falon <47285315+falOn-Dev@users.noreply.github.com>
2023-11-13 18:33:06 -0500

    Lesson 1.2 Created

---
+++ src/main/kotlin/Lesson1_2/Lesson1-2.kt
@@ -0,0 +1,10 @@
+package Lesson1_2
+
+fun main(){
+    /*
+    - Create four integers
+    - Print out if integer 1 is greater than integer 2
+    - Print out if integer 3 is less than or equal to integer 4
+    - Print out if integer 1 is not equal to integer 3 AND if integer 2 is equal to integer 4
+     */
+}
