a138cd20ed37787e20e911a84b9acb3b2792930d
Falon <47285315+falOn-Dev@users.noreply.github.com>
2023-11-19 17:20:34 -0500

    Created lesson 1.3 for logic statements

---
+++ src/main/kotlin/Lesson1_3/Lesson1-3.kt
@@ -0,0 +1,10 @@
+package Lesson1_3
+
+fun main(){
+    /*
+    - Create two integers
+    - Create an if statement that prints out "Integer 1 is greater than Integer 2" if integer 1 is greater than integer 2
+    - Use an else if statement to print out "Integer 1 is less than Integer 2" if integer 1 is less than integer 2
+    - Use an else statement to print out "Integer 1 is equal to Integer 2" if integer 1 is equal to integer 2
+     */
+}
