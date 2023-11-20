ecbe86631a91816734866d2bb08e40d7dc0a0bb4
Falon <47285315+falOn-Dev@users.noreply.github.com>
2023-11-20 13:38:18 -0500

    Created lesson 2.5 for data classes

---
+++ src/main/kotlin/Unit2/Lesson2_5/Lesson2-5.kt
@@ -0,0 +1,16 @@
+package Unit2.Lesson2_5
+
+/*
+- Create a data class called `UserAccount` with the following properties:
+    - `username` (String)
+    - `password` (String)
+    - `email` (String)
+    - `age` (Int)
+- Print the `toString()` of the data class
+- Create a copy of the data class with a new email address
+- Print the `toString()` of the copy
+ */
+
+fun main() {
+    // Place your code here
+}
