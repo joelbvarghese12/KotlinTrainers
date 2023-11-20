75d17b5fbfb5622f4b7609545acfdecc9fe353cf
Falon <47285315+falOn-Dev@users.noreply.github.com>
2023-11-20 13:44:29 -0500

    Created lesson 2.6 for singletons

---
+++ src/main/kotlin/Unit2/Lesson2_6/Lesson2-6.kt
@@ -0,0 +1,13 @@
+package Unit2.Lesson2_6
+
+/*
+- Create a singleton called `President` with the following properties:
+    - `name` (String)
+    - `age` (Int)
+    - `country` (String)
+- Print the `toString()` of the singleton
+*/
+
+fun main() {
+    // Place your code here
+}
