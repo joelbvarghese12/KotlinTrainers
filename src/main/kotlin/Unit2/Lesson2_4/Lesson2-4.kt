2bfc463bbdce708f074771de4b0e57a3b83701f8
Falon <47285315+falOn-Dev@users.noreply.github.com>
2023-11-20 13:22:07 -0500

    Created 2.4 for inheritance

---
+++ src/main/kotlin/Unit2/Lesson2_4/Lesson2-4.kt
@@ -0,0 +1,10 @@
+package Unit2.Lesson2_4
+
+/*
+- Create a base class of `Animal` with a property of `name` and a method of `makeNoise()`
+- Create a subclass of `Animal` called `Dog` that overrides the `makeNoise()` method to print "Woof!"
+- Create a subclass of `Animal` called `Cat` that overrides the `makeNoise()` method to print "Meow!"
+- Create instances of `Dog` and `Cat` and call their `makeNoise()` methods
+ */
+
+fun main() {}
